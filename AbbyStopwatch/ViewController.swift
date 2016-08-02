89740bcf34eb795bcbdade29fabf8f793896faea
thefreshduke <scottyshaw1@gmail.com>
2016-08-02 16:24:01 -0700

    Added alternateButtons()

--- AbbyStopwatch/ViewController.swift
+++ AbbyStopwatch/ViewController.swift
@@ -10,26 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var startButton: UIButton!
     @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var stopButton: UIButton!
     
     @IBAction func startTime(sender: AnyObject) {
         timeLabel.text = "START"
+        alternateButtons()
     }
     
     @IBAction func stopTime(sender: AnyObject) {
         timeLabel.text = "STOP"
+        alternateButtons()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        stopButton.enabled = false
+        stopButton.hidden = true
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func alternateButtons() {
+        startButton.enabled = !startButton.enabled
+        startButton.hidden = !startButton.hidden
+        stopButton.enabled = !stopButton.enabled
+        stopButton.hidden = !stopButton.hidden
+    }
 }
 
